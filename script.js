371b23e1e5344c51d99765d658ef090b1fc11464
JacksonRCode <jackdareid@gmail.com>
2023-08-22 13:35:15 -0400

    Make buttons functional and connect to calculation
    Buttons all work and will make calculations as a regular
    calculator would. Add booleans to ensure there aren't any mistakes
    such as multiple decimals in a number, multiple operators in an
    operation, calculating before there are two numbers, etc..

--- script.js
+++ script.js
@@ -22,6 +22,97 @@ function operate(first, item, second) {
     return item(parseInt(first), parseInt(second));
 }
 
+function clickReceiver(value) {
+    if (!isNaN(value)) {
+        addValue(value);
+    }
+    else if (value==='.' && !usedDot) {
+        addValue(value);
+        usedDot=true;
+    }
+    else if (value==='DEL') {
+        del();
+    }
+    else if (value==='AC') {
+        first=undefined;
+        second=undefined;
+        item=undefined;
+        onFirst=true;
+        usedDot=false;
+        choseItem=false;
+    }
+    else if (!choseItem && value!=='.' && value!=='=') {
+        item = value;
+        onFirst = false;
+        usedDot=false;
+        choseItem=true;
+    }
+    else if ((second) && value==='='){
+        result = sendItem();
+        console.log(result);
+    }
+    console.log(first, item, second);
+}
+
+function addValue(value) {
+    if (!first) first=value;
+        else if (onFirst) first += value;
+        else {
+            if (!second) second = value;
+            else second += value;
+        }
+}
+
+function del() {
+    if (second) {
+        if (second.indexOf('.')===second.length-1){
+            usedDot=false;
+        }
+        second = second.substr(0, second.length-1);
+    }
+    else if (item) {
+        item=undefined;
+        choseItem=false;
+    }
+    else if(first) {
+        if (first.indexOf('.')===first.length-1){
+            usedDot=false;
+        }
+        first = first.substr(0, first.length-1);
+    }
+}
+
+function sendItem() {
+    let result;
+        if (item==='+') {
+            result = operate(first, add, second);
+        }
+        else if (item==='-') {
+            result = operate(first, subtract, second);
+        }
+        else if (item==='x') {
+            result = operate(first, multiply, second);
+        }
+        else {
+            result = operate(first, divide, second);
+        }
+
+    return result;
+}
+
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        clickReceiver(btn.textContent);
+    });
+});
+
 let first;
 let item;
 let second;
+
+let onFirst = true;
+let usedDot = false;
+choseItem = false;
